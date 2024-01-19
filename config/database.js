442c8c800a289ebe3d9b18f4d636bc19a236567a
KAUÃ RODRIGUES MELLATO <kaua_mellato@estudante.sesisenai.org.br>
2024-01-19 14:58:31 -0300

    doc: criação da documentação da pagina database

--- config/database.js
+++ config/database.js
@@ -1,13 +1,17 @@
-const { Sequelize } = require('sequelize');
+// Fazendo a requisição do modulo Sequelize
+const { Sequelize } = require("sequelize");
 
-const dbName = 'Livraria';
-const dbUser = 'root';
-const dbPassorword = '';
+// Fazendo a armagenagem das configurações do banco de dados
+const dbName = "Livraria";
+const dbUser = "root";
+const dbPassorword = "";
 const dbHost = "localhost";
 
+// Criando uma instancia da classe Sequelize
 const sequelize = new Sequelize(dbName, dbUser, dbPassorword, {
+  //Configurando o host e dialeto(banco que sera utilizado)
   host: dbHost,
-  dialect: 'mysql'
+  dialect: "mysql",
 });
 
 module.exports = sequelize;
