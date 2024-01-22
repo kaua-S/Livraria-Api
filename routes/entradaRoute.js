936e6f3460e1ecf5f33d88fd4727655ce99c24a9
KAUÃ RODRIGUES MELLATO <kaua_mellato@estudante.sesisenai.org.br>
2024-01-22 15:34:00 -0300

    Feat:criando as rotas do EntradaEstoque

---
+++ routes/entradaRoute.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const EntradaController = require("../controllers/EntradaController");
+
+//Rota para criar um novo produto
+router.post("/entradas", EntradaController.createEntrada);
+
+//Rota para obter todos os produtos
+router.get("/entradas", EntradaController.getAllEntradas);
+
+//Rota para obter um produto pelo ID
+router.get("/entradas/:id", EntradaController.getEntradasById);
+
+//Rota para atualizar um produto
+router.put("/entradas/:id", EntradaController.updateEntrada);
+
+//Rota para deletar um produto
+router.delete("/entradas/:id", EntradaController.deleteEntranda);
+
+module.exports = router;
