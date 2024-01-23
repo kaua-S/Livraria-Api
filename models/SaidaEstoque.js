525e5c201627e13d01fe1af9aca40815a60afce9
Kaua-S <kaua_mellato@estudante.sesisenai.org.br>
2024-01-23 10:46:38 -0300

    Style: Modificando a classe SaidaEstoque para constante

--- models/SaidaEstoque.js
+++ models/SaidaEstoque.js
@@ -1,23 +1,20 @@
-const { DataTypes } = require('sequelize');
+const{Model , DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const { belongsTo } = require('./Produto');
+const Produto = require('./Produto');
 
-const SaidaEstoque = sequelize.define("saida_estoque", {
-    id_saida: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+const  SaidaEstoque = sequelize.define("saida_estoque",{
+    id_Saida:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
     },
-    quantidade: {
-        type: DataTypes.INTEGER,
-      
+    quantidade:{
+        type:DataTypes.INTEGER
     },
-    data_saida: {
-        type: DataTypes.DATE,
-      
+    data_saida:{
+    type:DataTypes.DATE
     }
-}, {
-    timestamps: false
-})
+}) 
 
 
-module.exports = SaidaEstoque;
