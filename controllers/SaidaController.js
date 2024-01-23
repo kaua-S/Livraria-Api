532ac452fec60a10e5188b02ff64d4b733d9a375
Kauã Mellato <126474688+kaua-S@users.noreply.github.com>
2024-01-23 16:56:03 -0300

    fix:  desconstruindo o modulo SaidaEstoque para se adicionar a chave estrangeira

--- controllers/SaidaController.js
+++ controllers/SaidaController.js
@@ -23,7 +23,7 @@ const SaidaController = {
         data_saida,
         id_produto
       });    
-      res.json(novaEntrada);
+      res.json(novaSaida);
     } catch (error) {
       res.status(500).send(error.message);
     }
