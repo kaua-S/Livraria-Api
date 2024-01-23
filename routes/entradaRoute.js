ab0b37b796a4eb61928e8794f671f5635b270a32
Kaua-S <kaua_mellato@estudante.sesisenai.org.br>
2024-01-23 11:22:25 -0300

    Test: Readicionando a rota create na EntradaRoute

--- routes/entradaRoute.js
+++ routes/entradaRoute.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const EntradaController = require("../controllers/EntradaController");
+const ProdutoController = require('../controllers/ProdutoController')
 
-
+router.post("/entradas", EntradaController.createEntrada);
 
 //Rota para obter todos os produtos
 router.get("/entradas", EntradaController.getAllEntradas);
 
 //Rota para obter um produto pelo ID
-router.get("/entradas/:id", EntradaController.getEntradasById);
+router.get("/entradas/:id", EntradaController.getEntradasById );
 
 //Rota para atualizar um produto
 router.put("/entradas/:id", EntradaController.updateEntrada);
